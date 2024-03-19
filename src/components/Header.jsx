5df960531d2a1216e6c1c4b023fed69bb6e27173
João Vitor <118351277+vso08@users.noreply.github.com>
2024-03-19 20:55:55 -0300

    Your commit message here

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,42 @@
+import React, { useEffect, useState } from 'react';
 import reactLogo from '../assets/react.svg';
 
-
-
-
 const Header = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollPosition = window.scrollY;
+            if (scrollPosition > 0) {
+                setIsScrolled(true);
+            } else {
+                setIsScrolled(false);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll);
+    }, []);
 
     const BackgroundColor = {
-        background: 'rgba(17,17,17,255)'
-    }
+        background: isScrolled ? 'rgba(17, 17, 17, 0.9)' : 'rgba(17, 17, 17, 1)',
+        transition: 'background-color 0.3s ease-in-out'
+    };
 
     const NavLgBgColor = {
         background: 'rgba(0, 0, 0, 0.9)',
         padding: '0',
         margin: '0'
-    }
+    };
 
     return (
-
-        <header className=''>
+        <header>
             <nav className="border-black flex justify-between relative lg:fixed w-screen z-5 x-" style={BackgroundColor}>
-                <div className="Logo ps-20 lg:ps-40">
-                    <img src={reactLogo} style={{ width: '50px', height: '50px' }} alt="React Logo" />
+                <div className="Logo ps-0 xl:ps-40 md:ps-40 sm:ps-20">
+                    <img src={reactLogo} className='w-12 h-12' alt="React Logo" />
                 </div>
                 <div className='w-full justify-between hidden lg:block pe-20 lg:pe-40 '>
-                    <ul className="list-none p-3 gap-40 visible flex flex-row w-full justify-end text-white">
+                    <ul className="list-none p-3 gap-20 xl:gap-40 visible flex flex-row w-full justify-end text-white text-lg xl:text-xl">
                         <li>
                             <a href="" className=''>História</a>
                         </li>
@@ -42,8 +55,8 @@ const Header = () => {
                 </div>
             </nav>
 
-            <nav className="flex justify-between w-screen fixed lg:relative text-white" style={NavLgBgColor}>
-                <div className='w-full justify-between visible lg:hidden pe-20 lg:pe-40 '>
+            <nav className={`fle w-screen fixed lg:relative text-white ${isScrolled ? 'top-0' : 'top-12'}`} style={NavLgBgColor}>
+                <div className='w-full visible lg:hidden pe-20 lg:pe-40'>
                     <ul className="list-none p-3 gap-20 lg:gap-40 visible flex flex-row w-full justify-end">
                         <li>
                             <a href="" className=''>História</a>
@@ -62,7 +75,6 @@ const Header = () => {
                     </ul>
                 </div>
             </nav>
-
         </header>
     );
 };
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,16 +1,19 @@
 
 
 function Menu() {
+
+
+
     return (
         <main className="pt-14 w-screen h-screen">
-            <div className="border container mx-auto px-20 mt-20 rounded-lg">
+            <div className="container mx-auto px-20 mt-20 rounded-lg">
                 <div className="banner grid columns-auto">
                     <div className="py-20 w-1/2 text-center rounded-sm bg-gradient-to-r from-transparent to-zinc-700">
-                        <h1 className="text-6xl  font-thin me-5 text-white">Se desafie contra inimigos e mundos poderosos! </h1>
-                        <button href="" className="border rounded-xl px-20 p-5 mt-20">Download</button>
+                        <h1 className="text-4xl sm:text-5xl md:text-6xl  font-thin me-5 text-white">Se desafie contra inimigos e mundos poderosos! </h1>
+                        <button href="" className="btn text-white bg-gradient-to-r from-yellow-300 to-red-600 outline outline-black outline-4 border-black rounded-xl px-10 md:px-20 p-5 mt-20 "
+                        >Download</button>
                     </div>
                     <div className="w-1/2 bac">
-
                     </div>
                 </div>
             </div>
